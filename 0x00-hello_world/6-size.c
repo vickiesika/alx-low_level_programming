b765f49dc85eebe586877b66c78b5b103c83e037
vickiesika <torisika317@gmail.com>
2023-02-16 17:17:37 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int intType
+	double doubleType;
+	char charType;
+	float floatType;
+
+	printf("Size of int: %zu bytes\n", size of(intType));
+	printf("Size of double: %zu bytes\n", size of(doubleType));
+	printf("Size of char: %zu bytes\n", size of(charType));
+	printf("Size of float: %zu bytes\n", size of(floatType));
+
+	return (0);
+}
