bc3163e1e9b5e4b3a11aed2076adbc85613675f5
vickiesika <torisika317@gmail.com>
2023-02-17 03:18:05 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	char info = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", info);
+
+	return (1);
+}
