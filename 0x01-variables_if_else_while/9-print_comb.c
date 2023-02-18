47177fb26caa45244e36541ab155829454c4b834
vickiesika <torisika317@gmail.com>
2023-02-17 18:12:57 -0800

    Prints combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible combinations of single digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(',');
+		putchar(',');
+	}
+	c++;
+	return (0);
+}
