40698aff9eef1d74fd9378225c732a6a070965aa
vickiesika <torisika317@gmail.com>
2023-02-23 06:49:26 -0800

    ten times a number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; i++)
+	for (a = 0; a < 10; a++)
 	{
-	for (b = 0; b < 15; b++)
+	for (b = 0; b <= 14; b++)
 	{
-	if (b >= 10)
-		_putchar(b / 10 + '0');
-		_putchar(b % 10 + '0');
+	if (b > 9)
+		_putchar((b / 10) + '0');
+		_putchar((b % 10) + '0');
 	}
 	_putchar('\n');
 	}
