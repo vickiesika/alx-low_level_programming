f3b91bb33b08d37cdd01de160059226f8f565f36
vickiesika <torisika317@gmail.com>
2023-02-23 06:23:56 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c <= 9; c++)
+	for (c = 48; c < 58; c++)
 	{
-		if (c != 2)
+		if (c != 50)
 		{
-		if (c != 4)
+		if (c != 52)
 		{
 		_putchar(c);
 		}
