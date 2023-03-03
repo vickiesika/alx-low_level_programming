5e2c20964fdc7f08ee1c77d0651f51c9379d5b1d
vickiesika <torisika317@gmail.com>
2023-03-03 08:11:29 -0800

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,25 +12,24 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-
-			i++;
-			if (str[i - 1] == ' ' ||
-				str[i - 1] == 't' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == "" ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-			i == 0)
-			str[i] -= 32;
+	while (!(str[i] >= 'a' && str[i] <= 'z'))
 		i++;
+	if (str[i - 1] == ' ' ||
+		str[i - 1] == 't' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+	i == 0)
+		str[i] -= 32;
+	i++;
 	}
 	return (str);
 }
